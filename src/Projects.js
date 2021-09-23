316767a510c33df941834a3e78a7398767790e7f
sherinececil <sherinececil@gmail.com>
2021-09-23 12:33:27 +0200

    correct responsiveness projects

--- src/Projects.js
+++ src/Projects.js
@@ -57,95 +57,100 @@ export default function Projects({ handleChange, checked }) {
   return (
     <div className={classes.root}>
       <div className={classes.container}>
-        <Grow in={checked}>
-          <Paper
-            elevation={4}
-            className={classes.paper}
-            style={styles.paperContainer2}
-          >
-            {" "}
-            <a href="https://nervous-bose-e8cd03.netlify.app/" target="_blank">
-              <svg className={classes.svg}></svg>
-            </a>
-            <h4>Know Your IP App</h4>
-            <a href="https://github.com/sherinececil/Ip-App" target="_blank">
-              <GitHubIcon />{" "}
-            </a>
-          </Paper>
-        </Grow>
+        <ul>
+          <Grow in={checked}>
+            <Paper
+              elevation={4}
+              className={classes.paper}
+              style={styles.paperContainer2}
+            >
+              {" "}
+              <a
+                href="https://nervous-bose-e8cd03.netlify.app/"
+                target="_blank"
+              >
+                <svg className={classes.svg}></svg>
+              </a>
+              <h4>Know Your IP App</h4>
+              <a href="https://github.com/sherinececil/Ip-App" target="_blank">
+                <GitHubIcon />{" "}
+              </a>
+            </Paper>
+          </Grow>
 
-        {/* Conditionally applies the timeout prop to change the entry speed. */}
-        <Grow
-          in={checked}
-          style={{ transformOrigin: "0 0 0" }}
-          {...(checked ? { timeout: 1000 } : {})}
-        >
-          <Paper
-            elevation={4}
-            className={classes.paper}
-            style={styles.paperContainer1}
+          {/* Conditionally applies the timeout prop to change the entry speed. */}
+          <Grow
+            in={checked}
+            style={{ transformOrigin: "0 0 0" }}
+            {...(checked ? { timeout: 1000 } : {})}
           >
-            {" "}
-            <a href="https://vibrant-boyd-67473f.netlify.app" target="_blank">
-              <svg className={classes.svg}></svg>{" "}
-            </a>
-            <h4>Hacker news</h4>
-            <a
-              href="https://github.com/sherinececil/HackerNews"
-              target="_blank"
+            <Paper
+              elevation={4}
+              className={classes.paper}
+              style={styles.paperContainer1}
             >
-              <GitHubIcon />
-            </a>
-          </Paper>
-        </Grow>
-        <Grow in={checked}>
-          <Paper
-            elevation={4}
-            className={classes.paper}
-            style={styles.paperContainer3}
-          >
-            <a
-              href="https://vibrant-wescoff-1211b0.netlify.app/"
-              target="_blank"
+              {" "}
+              <a href="https://vibrant-boyd-67473f.netlify.app" target="_blank">
+                <svg className={classes.svg}></svg>{" "}
+              </a>
+              <h4>Hacker news</h4>
+              <a
+                href="https://github.com/sherinececil/HackerNews"
+                target="_blank"
+              >
+                <GitHubIcon />
+              </a>
+            </Paper>
+          </Grow>
+          <Grow in={checked}>
+            <Paper
+              elevation={4}
+              className={classes.paper}
+              style={styles.paperContainer3}
             >
-              <svg className={classes.svg}></svg>{" "}
-            </a>
-            <h4>Healthy Twist App</h4>
-            <a
-              href="https://github.com/sherinececil/Healthy-Twist"
-              target="_blank"
-            >
-              <GitHubIcon />
-            </a>
-          </Paper>
-        </Grow>
+              <a
+                href="https://vibrant-wescoff-1211b0.netlify.app/"
+                target="_blank"
+              >
+                <svg className={classes.svg}></svg>{" "}
+              </a>
+              <h4>Healthy Twist App</h4>
+              <a
+                href="https://github.com/sherinececil/Healthy-Twist"
+                target="_blank"
+              >
+                <GitHubIcon />
+              </a>
+            </Paper>
+          </Grow>
 
-        <Grow
-          in={checked}
-          style={{ transformOrigin: "0 0 0" }}
-          {...(checked ? { timeout: 1000 } : {})}
-        >
-          <Paper
-            elevation={4}
-            className={classes.paper}
-            style={styles.paperContainer4}
+          <Grow
+            in={checked}
+            style={{ transformOrigin: "0 0 0" }}
+            {...(checked ? { timeout: 1000 } : {})}
           >
-            <a
-              href="https://romantic-banach-56d860.netlify.app/"
-              target="_blank"
+            <Paper
+              elevation={4}
+              className={classes.paper}
+              style={styles.paperContainer4}
             >
-              <svg className={classes.svg}></svg>
-            </a>
+              <a
+                href="https://romantic-banach-56d860.netlify.app/"
+                target="_blank"
+              >
+                <svg className={classes.svg}></svg>
+              </a>
 
-            <h4>ToDo List</h4>
-            <a
-              href="https://github.com/sherinececil/To-do-list-app"
-              target="_blank"
-            >
-              <GitHubIcon />
-            </a>
-          </Paper>
-        </Grow>
+              <h4>ToDo List</h4>
+              <a
+                href="https://github.com/sherinececil/To-do-list-app"
+                target="_blank"
+              >
+                <GitHubIcon />
+              </a>
+            </Paper>
+          </Grow>
+        </ul>
       </div>
     </div>
   );
